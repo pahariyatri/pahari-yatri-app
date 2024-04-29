6ef42e652c98cc21268f55db8957c364673af32d
pankaj-kumar-techie <pankajkumar.techie@gmail.com>
2024-04-30 00:50:02 +0530

    new file:   app/about/page.tsx 	new file:   app/tag/page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,6 @@
+
+export default async function About() {
+  return (
+    <h1 className="text-3xl font-bold underline">Hello, About Page !</h1>
+  );
+}
---
+++ app/tag/page.tsx
@@ -0,0 +1,6 @@
+
+export default async function Tag() {
+  return (
+    <h1 className="text-3xl font-bold underline">Hello, Tag Page !</h1>
+  );
+}
