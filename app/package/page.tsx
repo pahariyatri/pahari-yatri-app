b91ff15528b74b40a6cbcce2e45c84c8ac7753d0
pankaj-kumar-techie <pankajkumar.techie@gmail.com>
2024-08-16 20:02:53 +0530

    modified:   app/package/page.tsx

--- app/package/page.tsx
+++ app/package/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import PackageCard from "@/components/cards/PackageCard";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -7,43 +7,23 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrig
 import { Slider } from "@/components/ui/slider";
 import { useState } from "react";
 
-// Dummy data for package cards
-const packageData = [
-  {
-    id: 1,
-    title: "Adventure in the Alps",
-    description: "Explore the breathtaking Alps with our guided adventure tour.",
-    imageSrc: "/static/images/pahari-yatri-banner.png",
-    href: "/package/adventure-trekking",
-    price: 1500,
-    duration: 10,
-    location: "Switzerland"
-  },
-  {
-    id: 2,
-    title: "Tropical Paradise Getaway",
-    description: "Relax and unwind in a luxurious tropical paradise.",
-    imageSrc: "/static/images/pahari-yatri-banner.png",
-    href: "/package/adventure-trekking",
-    price: 2000,
-    duration: 10,
-    location: "Maldives"
-  },
-  {
-    id: 3,
-    title: "Cultural Tour of Asia",
-    description: "Immerse yourself in the rich cultural heritage of Asia.",
-    imageSrc: "/static/images/pahari-yatri-banner.png",
-    href: "/package/adventure-trekking",
-    price: 1800,
-    duration: 14,
-    location: "Various"
-  },
-  // Add more dummy data as needed
-];
+import keystaticConfig from "@/keystatic.config";
+import { createReader } from "@keystatic/core/reader";
 
-export default function Package() {
-  const [selectedPriceRange, setSelectedPriceRange] = useState([0, 100]);
+const reader = createReader(process.cwd(), keystaticConfig);
+
+export default async function Package() {
+  const packageData = await reader.collections.packages.all();
+  const packages = packageData.map(pkg => ({
+    title: pkg.entry.title,
+    description: pkg.entry.excerpt,
+    imageSrc: pkg.entry.image,
+    href: `/package/${pkg.slug}`,
+    price: pkg.entry.price ?? 0,
+    location: pkg.entry.location,
+  }));
+
+  // const [selectedPriceRange, setSelectedPriceRange] = useState([0, 100]);
   return (
     <>
       <div className="pb-8">
@@ -57,36 +37,20 @@ export default function Package() {
       <div className="container mx-auto py-8 px-4 flex flex-col md:flex-row">
 
         {/* Filter section */}
-        <div className="md:w-1/4 pr-4">
+        {/* <div className="md:w-1/4 pr-4">
           <div className="pb-8">
             <h2 className="text-lg font-semibold mb-4">Filters</h2>
-            {/* Location filter */}
             <div className="mb-4">
               <Label htmlFor="picture">Location</Label>
               <Input type="text" placeholder="Search by location" />
             </div>
 
-            {/* Price filter */}
-            <div className="mb-4">
-              <Label htmlFor="picture">Price Range</Label>
-              <div className="flex items-center">
-                <Slider defaultValue={[330]} max={5000} step={300} onValueChange={value => setSelectedPriceRange(value)} />
-                <span className="ml-2">{`$${selectedPriceRange[0]} - $${selectedPriceRange[1]}`}</span>
-              </div>
-            </div>
 
-            {/* Duration filter */}
             <div className="mb-4">
               <Label htmlFor="picture">Duration (Days)</Label>
               <Input type="number" placeholder="Enter duration" />
-              {/* <input
-                type="number"
-                className="block w-full mt-1 px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 focus:outline-none focus:border-indigo-500 dark:focus:border-indigo-400"
-                placeholder="Enter duration"
-              /> */}
             </div>
 
-            {/* Activities filter */}
             <div className="mb-4">
               <Label htmlFor="picture">Activities</Label>
               <Select>
@@ -95,7 +59,6 @@ export default function Package() {
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    {/* <SelectLabel>Activities</SelectLabel> */}
                     <SelectItem value="Hiking">Hiking</SelectItem>
                     <SelectItem value="Adventure">Adventure</SelectItem>
                     <SelectItem value="Sightseeing">Sightseeing</SelectItem>
@@ -103,31 +66,28 @@ export default function Package() {
                 </SelectContent>
               </Select>
             </div>
-
-            {/* Add more filter options if required */}
           </div>
-        </div>
+        </div> */}
 
         {/* Package listing section */}
-        <div className="md:w-3/4">
+        {/* <div className="md:w-3/4"> */}
           {/* Package cards section */}
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {/* Render Package cards */}
-            {packageData.map((packageItem) => (
+            {packages.map((packageItem) => (
               <PackageCard
-                key={packageItem.id}
+                key={packageItem.title}
                 title={packageItem.title}
                 description={packageItem.description}
                 imageSrc={packageItem.imageSrc}
-                href={packageItem.href}
-                price={packageItem.price}
-                duration={packageItem.duration}
+                href={''}
+                price={packageItem.price || 0}
                 location={packageItem.location}
               />
             ))}
           </div>
         </div>
-      </div>
+      {/* </div> */}
       <Pagination>
         <PaginationContent>
           <PaginationItem>
