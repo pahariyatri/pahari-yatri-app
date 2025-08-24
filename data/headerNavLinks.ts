c928fad109f2043effa76bd7db819c48d6122b59
pankaj-kumar-techie <pankajkumar.techie@gmail.com>
2025-08-24 22:45:43 +0530

    feat: Remove unused navigation links for cleaner header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,6 @@ const headerNavLinks = [
     { href: '/', title: 'Home' },
     { href: '/journeys', title: 'Journeys' },
     { href: '/stories', title: 'Stories' },
-    { href: '/why-pahari-yatri', title: 'Why Pahari Yatri' },
-    { href: '/culture', title: 'Culture' },
 ];
 
 export default headerNavLinks;
